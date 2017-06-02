4a7c90ee6b541232fca82b73b9684303ffe7fafc
pneff <pneff@unicon.net>
2017-06-02 15:05:24 -0700

    changed to map of friends

--- src/App.js
+++ src/App.js
@@ -4,16 +4,41 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to github battle</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+
+      var users = [
+        { name: 'Tyler', friend: true },
+        { name: 'Ryan', friend: true },
+        { name: 'Michael', friend: false },
+        { name: 'Mikenzi', friend: false },
+        { name: 'Jessica', friend: true },
+        { name: 'Dan', friend: false } ];
+
+      var friends = users.filter(function (item) {
+          return item.friend === true
+      });
+      var enemies = users.filter(function (item) {
+          return item.friend === false
+      });
+
+      return (
+          <div>
+              <h1>Friends</h1>
+              <ul>
+                  {friends.map(function(item) {
+                      return <li key={item.name}> {item.name} </li>
+                  })}
+              </ul>
+
+              <hr />
+
+              <h1> Non Friends </h1>
+              <ul>
+                  {enemies.map(function(item) {
+                      return <li key={item.name}> {item.name} </li>
+                  })}
+              </ul>
+          </div>
     );
   }
 }
