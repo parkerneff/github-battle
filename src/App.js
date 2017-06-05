f4026327382f475628c489e080c3723264eeb1e0
pneff <pneff@unicon.net>
2017-06-05 15:25:17 -0700

    Popular.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+var Popular = require('./Popular');
 
 class App extends Component {
   render() {
 
 
-      var users = [
-        { name: 'Tyler', friend: true },
-        { name: 'Ryan', friend: true },
-        { name: 'Michael', friend: false },
-        { name: 'Mikenzi', friend: false },
-        { name: 'Jessica', friend: true },
-        { name: 'Dan', friend: false } ];
-
-      var friends = users.filter(function (item) {
-          return item.friend === true
-      });
-      var enemies = users.filter(function (item) {
-          return item.friend === false
-      });
 
       return (
-          <div>
-              <h1>Friends</h1>
-              <ul>
-                  {friends.map(function(item) {
-                      return <li key={item.name}> {item.name} </li>
-                  })}
-              </ul>
-
-              <hr />
-
-              <h1> Non Friends </h1>
-              <ul>
-                  {enemies.map(function(item) {
-                      return <li key={item.name}> {item.name} </li>
-                  })}
-              </ul>
+          <div className="container">
+              <Popular/>
           </div>
+
     );
   }
 }
